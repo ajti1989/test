3b3806530b4bec4317a1230c398f44523a9490d8
antonio <ajti_1989@hotmail.com>
2023-11-08 08:46:49 +0100

    mi primer commit

---
+++ src/app/(emprendedores)/emprendedor1/page.js
@@ -0,0 +1,5 @@
+export default function Emprendedor1 (){
+    return(
+        <div><h1>Emprendedor1</h1></div>
+    )
+}
---
+++ src/app/(emprendedores)/emprendedor2/page.js
@@ -0,0 +1,8 @@
+export default function Emprendedor2(){
+    return(
+        <div>
+            <h1>Emprendedor2</h1>
+        </div>
+    )
+
+}
